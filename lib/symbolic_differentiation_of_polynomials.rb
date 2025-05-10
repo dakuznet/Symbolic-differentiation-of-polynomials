a01c79ed432c7f395f96624a1237e8e0d9bc8fe5
Daniil Kuznetsov <129334003+dakuznet@users.noreply.github.com>
2025-05-11 02:41:12 +0300

    generate gem

---
+++ lib/symbolic_differentiation_of_polynomials.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "symbolic_differentiation_of_polynomials/version"
+
+module SymbolicDifferentiationOfPolynomials
+  class Error < StandardError; end
+  # Your code goes here...
+end
